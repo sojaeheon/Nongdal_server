d7db43220eda88fddef1627980e9447c18d2e4f1
sojaeheon <sojaeheon123@daum.net>
2023-08-06 16:09:00 +0900

    왜 안돼

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//소재헌
+
 const mysql = require('mysql2');
 const express = require('express');
 
@@ -6,6 +8,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const session = require('express-session');
+
+// //test
+// app.post('/user/login', (req, res) => {
+//     try {
+//         console.log(req.body);
+        
+//     } catch (error) {
+//         console.error('에러가 발생했습니다:', error);
+//         res.json({
+//             'code': 500,
+//             'message': '서버 에러가 발생했습니다.'
+//         });
+//     }
+//   });
 
 /*
 const connection = mysql.createConnection({
@@ -16,11 +32,11 @@ const connection = mysql.createConnection({
 });
 */
 
-// mysql 
+mysql 
 const connection = mysql.createConnection({
-    host: '203.234.62.198',
-    user: 'uandi',
-    password: '1201',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '2049',
     database: 'ui_ms',
   });
 
@@ -50,18 +66,21 @@ app.post('/user/join', (req, res) => {
       const userPwd = req.body.UserPwd;
       const userEmail = req.body.UserEmail;
 
-      const sql = 'INSERT INTO users (UserId, UserPwd, UserEmail) VALUES (?, ?, ?)';
+      const sql = 'INSERT INTO users (id, password, email) VALUES (?, ?, ?)';
       const params = [userId, userPwd, userEmail];
 
       connection.query(sql, params, (err, result) => {
           let resultCode = 404;
           let message = '에러가 발생했습니다';
+          
 
           if (err) {
               console.log(err);
+              
           } else {
               resultCode = 200;
               message = '회원가입에 성공했습니다.';
+              
           }
 
           res.json({
@@ -81,35 +100,40 @@ app.post('/user/join', (req, res) => {
 //로그인
 app.post('/user/login', (req, res) => {
   try {
-      const userId = req.body.UserId;
-      const userPwd = req.body.UserPwd;
+    
+    const userId = req.body.UserId;
+      
+    const userPwd = req.body.UserPwd;
 
-      const sql = 'SELECT * FROM users WHERE UserId = ? AND UserPwd = ?';
 
-      connection.query(sql, [userId, userPwd], (err, result) => {
-          let resultCode = 404;
-          let message = '에러가 발생했습니다';
+    const sql = 'SELECT * FROM ui_ms.users WHERE id = ? AND password = ?';
 
-          if (err) {
-              console.log(err);
-          } else {
-              if (result.length > 0) {
-                  resultCode = 200;
-                  req.session.user = {
-                    UserId: result[0].UserId,
-                };
-                  message = '로그인 성공! ' + result[0].UserId + '님 환영합니다!';
-              }else {
-                resultCode = 204;
-                message = '로그인 실패!';
-              }     
-          }
+    connection.query(sql, [userId,userPwd], (err, result) => {
+        let resultCode = 404;
+        let message = '에러가 발생했습니다';
+        console.log(userId);
+        console.log(userPwd);
 
-          res.json({
-              'code': resultCode,
-              'message': message
-          });
-      });
+        if (err) {
+            console.log(err);
+        } else {
+            if (result.length > 0) {
+                resultCode = 200;
+                req.session.users = {
+                id: result[0].id,
+            };
+                message = '로그인 성공! ' + result[0].id + '님 환영합니다!';
+            }else {
+            resultCode = 204;
+            message = '로그인 실패!';
+            }     
+        }
+
+        res.json({
+            'code': resultCode,
+            'message': message
+        });
+    });
   } catch (error) {
       console.error('에러가 발생했습니다:', error);
       res.json({
@@ -212,78 +236,79 @@ app.post('/user/st', (req, res) => {
   });
   */
 
-  app.post('/user/memo', (req, res) => {
-    try {
-        console.log(req.body);
-        const sYear = req.body.sYear;
-        const sMonth = req.body.sMonth;
-        const sDate = req.body.sDate;
-        const stext = req.body.sText;
-        const sinput1 = req.body.sinput1;
-        const sinput2 = req.body.sinput2;
-        const sinput3 = req.body.sinput3;
+app.post('/user/memo', (req, res) => {
+try {
+    console.log(req.body);
+    const sYear = req.body.sYear;
+    const sMonth = req.body.sMonth;
+    const sDate = req.body.sDate;
+    const stext = req.body.sText;
+    const sinput1 = req.body.sinput1;
+    const sinput2 = req.body.sinput2;
+    const sinput3 = req.body.sinput3;
 
-        const selectSql = 'SELECT COUNT(*) AS count FROM strawdata WHERE syear = ? AND smonth = ? AND sdate = ?';
-        const selectParams = [sYear, sMonth, sDate];
+    const selectSql = 'SELECT COUNT(*) AS count FROM strawdata WHERE syear = ? AND smonth = ? AND sdate = ?';
+    const selectParams = [sYear, sMonth, sDate];
 
-        connection.query(selectSql, selectParams, (selectErr, selectResult) => {
-            if (selectErr) {
-                console.log(selectErr);
-                res.json({
-                    'code': 500,
-                    'message': '서버 에러가 발생했습니다.'
+    connection.query(selectSql, selectParams, (selectErr, selectResult) => {
+        if (selectErr) {
+            console.log(selectErr);
+            res.json({
+                'code': 500,
+                'message': '서버 에러가 발생했습니다.'
+            });
+        } else {
+            const count = selectResult[0].count;
+            if (count > 0) {
+                // 이미 데이터가 존재하는 경우, UPDATE 쿼리 실행
+                const updateSql = 'UPDATE strawdata SET sinput1 = ?, sinput2 = ?, sinput3 = ?, stext = ? WHERE syear = ? AND smonth = ? AND sdate = ?';
+                const updateParams = [sinput1, sinput2, sinput3, stext, sYear, sMonth, sDate];
+
+                connection.query(updateSql, updateParams, (updateErr, updateResult) => {
+                    if (updateErr) {
+                        console.log(updateErr);
+                        res.json({
+                            'code': 500,
+                            'message': '서버 에러가 발생했습니다.'
+                        });
+                    } else {
+                        res.json({
+                            'code': 200,
+                            'message': '수정에 성공했습니다.'
+                        });
+                    }
                 });
             } else {
-                const count = selectResult[0].count;
-                if (count > 0) {
-                    // 이미 데이터가 존재하는 경우, UPDATE 쿼리 실행
-                    const updateSql = 'UPDATE strawdata SET sinput1 = ?, sinput2 = ?, sinput3 = ?, stext = ? WHERE syear = ? AND smonth = ? AND sdate = ?';
-                    const updateParams = [sinput1, sinput2, sinput3, stext, sYear, sMonth, sDate];
+                // 데이터가 없는 경우, INSERT 쿼리 실행
+                const insertSql = 'INSERT INTO strawdata (syear, smonth, sdate, sinput1, sinput2, sinput3, stext) VALUES (?, ?, ?, ?, ?, ?, ?)';
+                const insertParams = [sYear, sMonth, sDate, sinput1, sinput2, sinput3, stext];
 
-                    connection.query(updateSql, updateParams, (updateErr, updateResult) => {
-                        if (updateErr) {
-                            console.log(updateErr);
-                            res.json({
-                                'code': 500,
-                                'message': '서버 에러가 발생했습니다.'
-                            });
-                        } else {
-                            res.json({
-                                'code': 200,
-                                'message': '수정에 성공했습니다.'
-                            });
-                        }
-                    });
-                } else {
-                    // 데이터가 없는 경우, INSERT 쿼리 실행
-                    const insertSql = 'INSERT INTO strawdata (syear, smonth, sdate, sinput1, sinput2, sinput3, stext) VALUES (?, ?, ?, ?, ?, ?, ?)';
-                    const insertParams = [sYear, sMonth, sDate, sinput1, sinput2, sinput3, stext];
-
-                    connection.query(insertSql, insertParams, (insertErr, insertResult) => {
-                        if (insertErr) {
-                            console.log(insertErr);
-                            res.json({
-                                'code': 500,
-                                'message': '서버 에러가 발생했습니다.'
-                            });
-                        } else {
-                            res.json({
-                                'code': 200,
-                                'message': '저장에 성공했습니다.'
-                            });
-                        }
-                    });
-                }
+                connection.query(insertSql, insertParams, (insertErr, insertResult) => {
+                    if (insertErr) {
+                        console.log(insertErr);
+                        res.json({
+                            'code': 500,
+                            'message': '서버 에러가 발생했습니다.'
+                        });
+                    } else {
+                        res.json({
+                            'code': 200,
+                            'message': '저장에 성공했습니다.'
+                        });
+                    }
+                });
             }
-        });
-    } catch (error) {
-        console.error('에러가 발생했습니다:', error);
-        res.json({
-            'code': 500,
-            'message': '서버 에러가 발생했습니다.'
-        });
-    }
+        }
+    });
+} catch (error) {
+    console.error('에러가 발생했습니다:', error);
+    res.json({
+        'code': 500,
+        'message': '서버 에러가 발생했습니다.'
+    });
+}
 });
+
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000/ 에서 실행 중입니다.');
 });
